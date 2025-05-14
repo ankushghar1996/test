28ec7a81f59e181810d5e308030d20d591e67db5
Ankush Gharsele <139872439+ankushghar1996@users.noreply.github.com>
2025-05-14 18:02:27 +0530

    Update Login_Credential.java

--- src/test/java/HO_LoginPage/Login_Credential.java
+++ src/test/java/HO_LoginPage/Login_Credential.java
@@ -16,16 +16,16 @@ public class Login_Credential extends BaseClass{
 	public void testloginpage() throws Exception {
 		LoginPage loginelements=PageFactory.initElements(driver, LoginPage.class);
 	
-		ObjectRepo.startTestAndLog_1_SS("Test Case 1", "Verify that user should be select Master Menu.");
+		ObjectRepo.startTestAndLog_1_SS("Test Case 1", "Verify that user should be send username.");
 		Liabrary.custom_Sendkeys(loginelements.getUserName(), excel.getStringdata("Sheet1", 1, 0), "UserName Field");
 		Thread.sleep(1000);
 		
 		
-		ObjectRepo.startTestAndLog_1_SS("Test Case 1", "Verify that user should be select Master Menu.");
+		ObjectRepo.startTestAndLog_1_SS("Test Case 1", "Verify that user should be send password.");
 		Liabrary.custom_Sendkeys(loginelements.getPassword(), excel.getStringdata("Sheet1", 1, 1), "Passwod Field");
 		Thread.sleep(1000);
 		
-		ObjectRepo.startTestAndLog_1_SS("Test Case 1", "Verify that user should be select Master Menu.");
+		ObjectRepo.startTestAndLog_1_SS("Test Case 1", "Verify that user should be click on Login button.");
 		Liabrary.custom_click(loginelements.getLogin_Btn(), "Login Btn");
 		Thread.sleep(1000);
 		
