7fb6287cda3775b039beed856a4778f361c56742
Ankush <ankush.gharsele@heerasoftware.com>
2025-07-21 14:32:14 +0530

    new21

--- src/test/java/SEBS_Portal_Credential/New_Distributor_Appointment.java
+++ src/test/java/SEBS_Portal_Credential/New_Distributor_Appointment.java
@@ -70,7 +70,7 @@ public class New_Distributor_Appointment {
 	    @Test
 	    public void testOtpFlowAndRetailerInfo() throws Exception {
 	    	
-	        String mobileNumber = "9082956707";
+	        String mobileNumber = "9082956708";
 
 	        // Login
 	        
@@ -179,7 +179,7 @@ public class New_Distributor_Appointment {
 	        Thread.sleep(1000);
 	        
 	        ObjectRepo.startTestAndLog_1_SS("SEBS_New_DIST_APPOINTMENT_TC_016", "Send  Distributor Firm Name ", () -> {
-	        driver.findElement(By.xpath("//input[@placeholder='Enter Distributor Firm Name']")).sendKeys("kanchan pal enterprises");});
+	        driver.findElement(By.xpath("//input[@placeholder='Enter Distributor Firm Name']")).sendKeys("pranamaya Deshpande enterprises");});
 	        Thread.sleep(1000);
 	        
 	       /* 
@@ -472,7 +472,7 @@ public class New_Distributor_Appointment {
 	            Connection conn = DriverManager.getConnection(url, username, password);
 	            Statement stmt = conn.createStatement();
 
-	            String query = "select * from RegistrationPendingMaster where MobileNo='9082956707'";
+	            String query = "select * from RegistrationPendingMaster where MobileNo='9082956708'";
 	            
 	            ResultSet rs = stmt.executeQuery(query);
 	            
