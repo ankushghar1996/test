13d55ca4b033bd2a2c6477eceec07de90d9a822e
Ankush Gharsele <139872439+ankushghar1996@users.noreply.github.com>
2025-08-12 11:35:47 +0530

    Update Demo_Mail.java

--- src/main/java/Com_Utility/Demo_Mail.java
+++ src/main/java/Com_Utility/Demo_Mail.java
@@ -77,11 +77,11 @@ public class Demo_Mail {
             // Add recipients
             email.addTo("aniket.jadhav@heerasoftware.com");
             email.addTo("ankush.gharsele@heerasoftware.com");
-//            email.addTo("roopali.kulkarni@heerasoftware.com");
-//			email.addTo("mahesh.kulkarni@heerasoftware.com");
-//			email.addTo("santosh.dhoot@heerasoftware.com");
-//			email.addTo("snehalata.patil@heerasoftware.com");
-//	           email.addTo("jidnyesh.borse@heerasoftware.com");
+            email.addTo("roopali.kulkarni@heerasoftware.com");
+         			email.addTo("mahesh.kulkarni@heerasoftware.com");
+         			email.addTo("santosh.dhoot@heerasoftware.com");
+         			email.addTo("snehalata.patil@heerasoftware.com");
+	           email.addTo("jidnyesh.borse@heerasoftware.com");
 //            // email.addTo("rohit.deshpande@heerasoftware.com");
  
 
